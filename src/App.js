ac2fcee24647955bb66e3c52a9c625446f8366d8
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-03-01 20:02:56 -0800

    conditional render content

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import './App.css'
 
 function App() {
   const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(null)
+  const [error, setError] = useState(null)
+
   const fetchMovies = async () => {
     setIsLoading(true)
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
-    const transformedData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
+
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if (!response.ok) {
+        throw new Error('Network Error')
       }
-    })
-    setMovies(transformedData)
+
+      const data = await response.json()
+
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        }
+      })
+      setMovies(transformedData)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error.message)
+    }
+
     setIsLoading(false)
   }
 
@@ -28,7 +43,12 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
+        {isLoading === null && <p>Search movies</p>}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {isLoading === false && movies.length === 0 && !error && (
+          <p>Movies not found</p>
+        )}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
