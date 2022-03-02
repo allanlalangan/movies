36341539feccd9e2371df4a0bdbea4822af6dad1
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-03-01 19:16:51 -0800

    new isLoading state. display message if isLoading

--- src/App.js
+++ src/App.js
@@ -4,23 +4,10 @@ import MoviesList from './components/MoviesList'
 import './App.css'
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ]
-
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const fetchMovies = async () => {
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json()
     const transformedData = data.results.map((movieData) => {
@@ -32,6 +19,7 @@ function App() {
       }
     })
     setMovies(transformedData)
+    setIsLoading(false)
   }
 
   return (
@@ -40,7 +28,8 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   )
